26543b84aa676150d7cc08dde2fb21470ef05fea
PML30\cgsg247 <alexatyukov30@gmail.com>
2026-06-10 17:51:05 +0300

    Added model loading & User Interface.

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,51 @@
+// const resolve = require("@rollup/plugin-node-resolve");
+// const commonjs = require("@rollup/plugin-commonjs");
+// const terser = require('@rollup/plugin-terser');
+
+// module.exports = {
+//     input: "src/client/main.js",
+//     output: {
+//         file: 'dist/bundle.js',
+//         format: "iife",
+//         //sourcemap: "inline",
+//     },
+//     plugins: [
+//         resolve({
+//             jsnext: true,
+//             main: true,
+//             browser: true,
+//         }),
+//         commonjs(),
+//         terser()
+//     ],
+// };
+
 const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const terser = require('@rollup/plugin-terser');
 
 module.exports = {
     input: "src/client/main.js",
+    external: ['three'],
     output: {
         file: 'dist/bundle.js',
         format: "iife",
-        sourcemap: "inline",
+        // 1. Изменяем 'inline' на true. Карта кода будет создаваться в ОТДЕЛЬНОМ файле bundle.js.map.
+        // Это мгновенно разгрузит оперативную память при сборке.
+        sourcemap: true,
     },
     plugins: [
         resolve({
-            jsnext: true,
-            main: true,
+            // Опции jsnext и main устарели, для современных версий плагина достаточно browser: true
             browser: true,
         }),
-        commonjs(),
-        terser()
+        commonjs()//,
+        // 2. Жестко ограничиваем Terser, чтобы он не перегружал процессор и RAM
+        // terser({
+        //     maxWorkers: 1, // Запускаем строго в один поток
+        //     compress: {
+        //         passes: 1, // Отключаем повторные циклы оптимизации
+        //     }
+        // })
     ],
 };
